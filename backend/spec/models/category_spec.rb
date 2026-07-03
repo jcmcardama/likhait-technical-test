f1fbe192b0d9cbdb3c10e8cbd3d19d966b39b4df
Jan Carlo Cardama <jcmcardama@gmail.com>
2026-07-04 00:45:41 +0800

    add model test and fix test

--- backend/spec/models/category_spec.rb
+++ backend/spec/models/category_spec.rb
@@ -1,4 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
+  it "is valid with valid attributes" do
+    category = Category.new(name: "Food")
+    expect(category).to be_valid
+  end
+
+  it "is invalid without a name" do
+    category = Category.new(name: "")
+    expect(category).not_to be_valid
+    expect(category.errors[:name]).to include("can't be blank")
+  end
 end
