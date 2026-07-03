7c4345ca238b566189eb89f5b7a5b1a94f7c8e23
Jan Carlo Cardama <jcmcardama@gmail.com>
2026-07-04 05:25:11 +0800

    add guard for different machine timezone

--- backend/spec/models/expense_spec.rb
+++ backend/spec/models/expense_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Expense, type: :model do
+  include ActiveSupport::Testing::TimeHelpers
+
   let(:category) { Category.create!(name: "Food") }
+
+  before do
+    travel_to Time.zone.local(2026, 7, 4, 12, 0, 0)
+  end
+
+  after do
+    travel_back
+  end
 
   it "is invalid with a future date" do
     expense = Expense.new(
